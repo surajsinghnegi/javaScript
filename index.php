58b888f8c6dd9dc126c283b27c50bb185e8a0c97
Suraj <surajsinghnegi@users.noreply.github.com>
2018-03-05 15:57:29 +0530

    first commit
    this is first ever commit to  this project

--- index.php
+++ index.php
@@ -26,8 +26,6 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/js/bootstrap-select.min.js"></script>
 
-
-
 </body>
 </html>
 
