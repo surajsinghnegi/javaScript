1332862bd45bfa58c736e6b9fe65e61d9e5b694c
Suraj <surajsinghnegi@users.noreply.github.com>
2018-03-05 16:48:18 +0530

    second commit

--- index.php
+++ index.php
@@ -1,31 +1,5 @@
-<html>
-  <head>
-   <title>Bootstrap Live Search Plugin - One of the Best Plugin Select Box with Live Search Option</title>  
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/css/bootstrap-select.min.css" />
-  </head>
-<body>
-  <div class="container">
-  <h2>One of the Best Plugin for Select Box with Data Live Search Option</h2>
-    <div class="row">
-      <select class="selectpicker" data-show-subtext="true" data-live-search="true">
-       <option>Expert PHP</option>
-        <option>Demo PHP</option>
-        <option>PHP Tutorials</option>
-        <option>PHP Framework</option>
-        <option>PHP Libraries</option>
-        <option>HTML</option>
-        <option>CSS</option>
-        <option>JS</option>
-        <option>MySQL</option>
-      </select>
-    
-    </div>
-  </div>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/js/bootstrap-select.min.js"></script>
+<?php 
 
-</body>
-</html>
+echo "hi";
 
+ ?>
